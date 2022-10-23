d63efe6e43aef9f64f0d9826e8a3f86b485e4aa3
Mohibul <mohib.hossain.bu@gmail.com>
2022-10-23 17:15:57 +0600

    added design and database code for CRUD operation of the list

--- index.js
+++ index.js
@@ -18,7 +18,9 @@ app.set("view engine", "ejs");
 
 // get method
 app.get("/", (req, res) => {
-  res.render("todo.ejs");
+  TodoTask.find({}, (err, tasks) => {
+    res.render("todo.ejs", { todoTasks: tasks });
+  });
 });
 
 // post method
@@ -26,6 +28,7 @@ app.post("/", async (req, res) => {
   const todoTask = new TodoTask({
     content: req.body.content,
   });
+  // console.log(todoTask);
   try {
     await todoTask.save();
     res.redirect("/");
@@ -34,6 +37,33 @@ app.post("/", async (req, res) => {
     console.log("POST Failed! :" + err);
   }
 });
+
+// update
+app
+  .route("/edit/:id")
+  .get((req, res) => {
+    const id = req.params.id;
+    TodoTask.find({}, (err, tasks) => {
+      res.render("todoEdit.ejs", { todoTasks: tasks, idTask: id });
+    });
+  })
+  .post((req, res) => {
+    const id = req.params.id;
+    TodoTask.findByIdAndUpdate(id, { content: req.body.content }, (err) => {
+      if (err) return res.send(500, err);
+      res.redirect("/");
+    });
+  });
+
+//DELETE
+app.route("/remove/:id").get((req, res) => {
+  const id = req.params.id;
+  TodoTask.findByIdAndRemove(id, (err) => {
+    if (err) return res.send(500, err);
+    res.redirect("/");
+  });
+});
+
 // database
 dotenv.config();
 //console.log(process.env);
